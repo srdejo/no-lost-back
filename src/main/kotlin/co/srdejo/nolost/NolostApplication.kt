d0ae0a7073cd6ef0f36552dd797dbc66a3094537
Daniel Jimenez <srdejo@gmail.com>
2025-03-19 11:50:34 -0500

    first commit

---
+++ src/main/kotlin/co/srdejo/nolost/NolostApplication.kt
@@ -0,0 +1,11 @@
+package co.srdejo.nolost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NolostApplication
+
+fun main(args: Array<String>) {
+	runApplication<NolostApplication>(*args)
+}
---
+++ src/test/kotlin/co/srdejo/nolost/NolostApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.srdejo.nolost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NolostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
